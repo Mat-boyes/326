e169fa13e68dcb05077e8e1c10a2a703d0125df1
Mika Smith <mika.e.smith@gmail.com>
2017-07-17 18:47:04 -0400

    Almost there! Not updating hashmap values for some reason so never considering if a cell is already visited and getting its state. Maybe initialise the hashmap with all possible values when the program starts?

--- src/cosc326/ants.java
+++ src/cosc326/ants.java
@@ -17,6 +17,7 @@ public class ants {
         private Point loc = new Point(0,0);
         private HashMap<Point, Character> visited = new HashMap<Point, Character>();
         private int lastDir = 0; //0 = north, 1= east, 2=south, 3=west
+        private ArrayList<Gene> genes = new ArrayList<Gene>();
 
 	public ants(){
 	}
@@ -54,9 +55,7 @@ public class ants {
             char state;
            // char[] states = new char[statesNum];
        //     ArrayList<Character> states = new ArrayList<Character>();
-            ArrayList<Gene> genes = new ArrayList<Gene>();
-            
-            
+
             Scanner scan = new Scanner(dna);
             while(scan.hasNextLine()){ //scan each line of the DNA 
             	String s = scan.nextLine();
@@ -72,63 +71,67 @@ public class ants {
             	genes.add(gene);
             }
             //lastDir
-            for(Gene g : genes){
+      /*      for(Gene g : genes){
             	System.out.println(g.getAction(lastDir)+" " + g.getNextState(lastDir));
-            }
+            }*/
             scan.close(); 
             
             System.out.println("Initial location: " + loc);
             
            
             for(int i = 0; i < stop; i++){ //for each step we need to make 
-                if(visited.get(loc) != null){ //if HAS been visited
+                if(visited.containsKey(loc)){ //if HAS been visited
                     state = visited.get(loc); //get the state of that location 
                  //   System.out.println(state);
+                    System.out.println("visited");
                 }else{//if it has NOT been visited, just use first state. 
                 	state = (genes.get(0)).getCurrentState();
+                	System.out.println("not visited");
                 }
              //   System.out.println("State: " + state);
                 //pass the state of the location and the direction coming from
-                movement(state, lastDir);
+                movement(state);
    
                 //UPDATE = state of new location is the currentstate of the last direction?          
-                visited.put(loc, genes.get(lastDir).getCurrentState()); //add the state of the location to the hashmap
             }
                 
             System.out.println("End location: " + loc);
             
         }
        
-       public void movement(char state, int lastDir){
+       public void movement(char state){
            //input state is the state to react to. 
     	   //input lastDir is the direction I've come from 0-3
     	   
-    	   //index of current state in dna
-           // int starter =dna.indexOf(state);
-            
-            //adds last direction to the state 
-            //starter += lastDir +1;
-            //char c = dna.charAt(starter); 
-    	   
-    	  
-    	  
-            if(lastDir==0){  //if came from North
+    	   //need to get the gene where the currentState matches state 
+    	   Gene g = new Gene();
+    	   for(Gene x: genes){
+    		   if(x.getCurrentState() == state){
+    			   g = x;  //this is our gene of interest
+    		   }
+    	   }
+    	   char nextDirection = g.getAction(lastDir);
+    	   char newState = g.getNextState(lastDir);
+    	 //update hashtable with new state
+           visited.put(loc, newState); //add the state of the location to the hashmap
+    	   System.out.println("Move in direction "+ nextDirection);
+    	   System.out.println("New state of "+ loc + " is " + newState);
+            if(nextDirection == 'N'){  //if came from North
+            	//go in direction g.actions(lastDir) and set state to g.
                 loc.translate(0, 1);
-                System.out.println("here!");
-            //    lastDir = 0;
-                
-            }else if(lastDir==1){
+                lastDir = 0;
+            }else if(nextDirection == 'E'){
                 loc.translate(1, 0);
-              //  lastDir = 1;
-            }else if(lastDir==3){
+                lastDir = 1;
+            }else if(nextDirection == 'S'){
                 loc.translate(0, -1);
-               // lastDir = 2;
+                lastDir = 2;
             }else{
                 loc.translate(-1, 0);
-               // lastDir = 3;
-            }
+                lastDir = 3;
+            } 
             
-         
+            
         }
         
       /*public void movement(char state, int lastDir){
@@ -161,6 +164,9 @@ public class ants {
     	   private char currState;
     	   private String actions;
     	   private String nextStates;
+    	   
+    	   public Gene(){   
+    	   }
     	   
     	   public Gene(char currState, String actions, String nextStates){
     		   this.currState = currState;
