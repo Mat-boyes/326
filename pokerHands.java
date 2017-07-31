3b69d5f5b5e0cbd8fd42fead6b01347ec54ab7a1
Mat-boyes <boyes.matthew@yahoo.co.nz>
2017-07-31 13:29:05 +1200

    Update pokerHands.java
    accets one line and trys to parse it making sure we have acceptable syntax for a hand

--- pokerHands.java
+++ pokerHands.java
@@ -1,1 +1,68 @@
 
+
+/*
+ * Etude 1: Ants on a Plane
+ * 
+ * Author: Mathew Boyes
+ * 
+ */
+
+import java.util.*;
+
+
+public class pokerHands{
+	
+	private String[] hand;
+	private String line, output;
+	
+	
+	public pokerHands(){
+		hand = new String[5];
+		
+	}
+	
+	public static void main(tring[]args){
+		Scanner scan = new Scanner(System.in);
+		
+		
+		//Hand hand = new Hand();
+		
+		line = scan.nextLine();
+		
+		setupHand(checkValid());
+		
+		
+		System.out.println(output);
+	}
+	
+	public String checkValid(){
+		
+		
+		
+		if(line.contains('-')){
+			return "-";
+		}
+		
+		if(line.contains('/')){
+			return "/";
+		}
+		
+		if(line.contains(' ')){
+			return " ";
+		}
+		return "invalid: ";
+		
+	}
+	
+	public void setupHand(String s){
+		int start = 0, end = 0, i = 0;
+		
+		while(start < line.length() || i < 5){
+			end = line.indexOf(s, start) - 1;
+			hand[i] = line.substring(start, end);
+			start = end+1;
+			i++;
+		}
+		
+	}
+}
